ae5185a90dd355cca26939b6a62c616c489f718e
Jim McLean <jim_mclean@optusnet.com.au>
2020-12-07 17:58:22 +1100

    Add mimicType column to CSVs. Add some stats reporting. Add (but comment out) capability to combine accuracy with the inverse likelihood of a 2nd pattern

--- MachineLearning/extract-accuracy.R
+++ MachineLearning/extract-accuracy.R
@@ -19,20 +19,14 @@ RAW_LABELS_FN <- "gv_raw_labels.csv"
 GOOGLE_VISION_IMAGE_LABELS_CSV <- file.path(OUTDIR, RAW_LABELS_FN)
 
 
-# Reads in pre-calculated Google image labels with scores, and
-# calculates an antLike score (i.e. mimetic accuracy) for each photo
-# as the highest probability associated with any label ending in the
-# word "ant".
-#
-# @returns Data frame with columns \code{imageUrl} and \code{accuracy}.
-ReadGoogleVisionImageAccuracy <- function() {
-  gv <- read.csv(GOOGLE_VISION_IMAGE_LABELS_CSV, stringsAsFactors = FALSE)
-  urls <- unique(gv$id)
+# Returns the highest-valued probability that matches pattern for each image in rawScores.
+BestMatchingProbability <- function(rawScores, pattern) {
+  urls <- sort(unique(rawScores$id))
   scores <- sapply(urls, function(url) {
-    labels <- gv[gv$id == url, ]
+    labels <- rawScores[rawScores$id == url, ]
     # Which label should we use? Pick the highest probability label
-    # that ends in the word "ant", e.g. could be "Carpenter ant"
-    candidates <- grep("^ant$|\\Want", labels$label, value = TRUE, ignore.case = TRUE)
+    # that 
+    candidates <- grep(pattern, labels$label, value = TRUE, ignore.case = TRUE)
     labels <- labels[labels$label %in% candidates, ]
     best <- which.max(labels$probability)
     # Accuracy is just the probability of the "best" label
@@ -41,6 +35,29 @@ ReadGoogleVisionImageAccuracy <- function() {
     ifelse (length(p) == 0, 0, p)
   })
   data.frame(imageUrl = names(scores), antLike = scores)
+}
+
+# Reads in pre-calculated Google image labels with scores, and
+# calculates an antLike score (i.e. mimetic accuracy) for each photo
+# as the highest probability associated with any label ending in the
+# word "ant".
+#
+# @returns Data frame with columns \code{imageUrl} and \code{accuracy}.
+ReadGoogleVisionImageAccuracy <- function() {
+  raw <- read.csv(GOOGLE_VISION_IMAGE_LABELS_CSV, stringsAsFactors = FALSE)
+  # This regular expression matches the label "ant", as well as labels that end
+  # in the word "ant", e.g. could be "Carpenter ant"
+  scores <- BestMatchingProbability(raw, "^ant$|\\Want$")
+
+  # A possible approach to improve the scores for poor mimics is to take the
+  # negative of their "spider-like" score, i.e. the more spider-like it is, the
+  # less ant-like it is.This may need to be fine-tuned for the application.
+  #
+  # This doesn't seem to improve the scores for our data, so we don't use it.
+  # spiderScores <- BestMatchingProbability(gv, "^spider$|\\Wspider$")
+  # scores <- ifelse(scores$antLike > 0, scores$antLike, 1 - spiderScores$antLike)
+  
+  scores
 }
 
 # Reads in the photo list, and manipulates it slightly to get it the
@@ -52,14 +69,14 @@ ReadPhotoPoolInfo <- function() {
   # Convert any type of lateral (e.g. "lateral right side") to lateral
   info$angle[grep("lateral", info$angle, ignore.case = T)] <- "Lateral"
   # Classify as ant, mimic or non-mimic
-  info$mimicType <- ifelse(info$family == "Formicidae", "ant", 
+  info$mimicType <- ifelse(info$family == "Formicidae", "model", 
                            ifelse(info$isMimic, "mimic", "non-mimic"))
   info
 }
 
 # Adds photo info to user scores.
 AddPhotoInfo <- function(scores, urlCol = "imageUrl") {
-  # Read in photo info (ant, mimic, non-mimic etc.)
+  # Read in photo info (model, mimic, non-mimic etc.)
   info <- ReadPhotoPoolInfo()
   
   # Match info to scores by matching URLs
@@ -80,7 +97,7 @@ AddPhotoInfo <- function(scores, urlCol = "imageUrl") {
 GoogleVisionSpeciesAngleStats <- function(gv,
                                           includeAngles = c("all", "Dorsal", "Lateral", "Posterior", "Anterior"),
                                           fun = mean,
-                                          byCols = c("class", "family", "species", "angle", "isMimic")) {
+                                          byCols = c("class", "family", "species", "angle", "mimicType")) {
   gv <- AddPhotoInfo(gv)
   
   # Optionally exclude some angles because google vision isn't very good at them
@@ -100,7 +117,9 @@ GoogleVisionSpeciesAngleStats <- function(gv,
 
 # Averages 
 GoogleVisionSpeciesStats <- function(gv, fun = mean, includeAngles = "all") {
-  GoogleVisionSpeciesAngleStats(gv, fun = fun, byCols = c("class", "family", "species",  "isMimic"), includeAngles = includeAngles)
+  GoogleVisionSpeciesAngleStats(gv, fun = fun, 
+                                byCols = c("class", "family", "species",  "mimicType"),
+                                includeAngles = includeAngles)
 }
 
 # Copies CSV files from one directory to another, excluding any that
@@ -125,7 +144,7 @@ gv <- ReadGoogleVisionImageAccuracy()
 
 ###
 # Save accuracy per image
-gv <- gv[order(gv$antLike), ]
+gv <- gv[order(gv$antLike, gv$imageUrl), ]
 write.csv(gv, "output/Machine learning-accuracy-images.csv", row.names = FALSE)
 
 ###
@@ -142,3 +161,15 @@ write.csv(sp, file.path(OUTDIR, "Machine learning-accuracy-species.csv"), row.na
 # Now copy all CSV files, apart from the raw labels file, to the global output directory
 CopyCsvs()
 
+
+#### 
+# Report some basic stats
+info <- AddPhotoInfo(gv)
+cat(sprintf("%d photos, %d mimics from %d species, %d ants (%d species, %s) and %d non-mimics (%d species, %s)\n",
+            nrow(info), 
+            sum(info$mimicType == "mimic"), length(unique(info$species[info$mimicType == "mimic"])),
+            sum(info$mimicType == "model"),  length(unique(info$species[info$mimicType == "model"])), 
+            info$species[info$mimicType == "model"][1],
+            sum(info$mimicType == "non-mimic"), length(unique(info$species[info$mimicType == "non-mimic"])),
+            info$species[info$mimicType == "non-mimic"][1]
+            ))
